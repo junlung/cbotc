7ff1c8adce23d7770ecd662fe8201012d30fcef9
Corn Boy <cornboyfresh@gmail.com>
2023-11-10 14:32:08 -0500

    fix port issue

--- modals/add_quote_modal.py
+++ modals/add_quote_modal.py
@@ -16,8 +16,8 @@ class AddQuoteModal(discord.ui.Modal, title='Add Quote'):
 
         async def on_submit(self, interaction: discord.Interaction):
             await interaction.response.send_message(f'Quote added: "{self.quote.value}" -{self.name.value}')
-            quote_params = { 'text': self.quote.value, 'person_name': self.quote.name }
-            requests.post("http://127.0.0.1:8000/quote", quote_params)
+            quote_params = { 'text': self.quote.value, 'person_name': self.name.value }
+            requests.post("http://172.20.0.2:8000/quote", json=quote_params)
 
         async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
             await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
