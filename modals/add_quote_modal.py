7f35d7db025e15573816c6a581848a79ee9594e8
Corn Boy <cornboyfresh@gmail.com>
2023-11-10 17:36:46 -0500

    fix internal networking

--- modals/add_quote_modal.py
+++ modals/add_quote_modal.py
@@ -17,7 +17,7 @@ class AddQuoteModal(discord.ui.Modal, title='Add Quote'):
         async def on_submit(self, interaction: discord.Interaction):
             await interaction.response.send_message(f'Quote added: "{self.quote.value}" -{self.name.value}')
             quote_params = { 'text': self.quote.value, 'person_name': self.name.value }
-            requests.post("http://172.20.0.2:8000/quote", json=quote_params)
+            requests.post("http://sanctum:8000/quote", json=quote_params)
 
         async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
             await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
